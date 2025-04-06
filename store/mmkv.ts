f534201364e9a4994881ff37a4d8e0903b66db7b
captainAmerica700 <milindpethkar011@gmail.com>
2025-04-06 09:56:03 +0530

    Refactor code structure for improved readability and maintainability

--- store/mmkv.ts
+++ store/mmkv.ts
@@ -1,36 +1,36 @@
-import { MMKV } from 'react-native-mmkv';
+// import { MMKV } from 'react-native-mmkv';
 
-export const storage = new MMKV();
+// export const storage = new MMKV();
 
-// Define types for keys and values
-type StorageKey = string;
-type StorageValue = string | number | boolean;
+// // Define types for keys and values
+// type StorageKey = string;
+// type StorageValue = string | number | boolean;
 
-// Helper functions for MMKV
-export const setItem = (key: StorageKey, value: StorageValue): void => {
-  if (typeof value === 'string') {
-    storage.set(key, value);
-  } else if (typeof value === 'number') {
-    storage.set(key, value);
-  } else if (typeof value === 'boolean') {
-    storage.set(key, value);
-  }
-};
+// // Helper functions for MMKV
+// export const setItem = (key: StorageKey, value: StorageValue): void => {
+//   if (typeof value === 'string') {
+//     storage.set(key, value);
+//   } else if (typeof value === 'number') {
+//     storage.set(key, value);
+//   } else if (typeof value === 'boolean') {
+//     storage.set(key, value);
+//   }
+// };
 
-export const getItem = (key: StorageKey): string | null => {
-  const storedValue = storage.getString(key);
-  if (!storedValue) return null;
+// export const getItem = (key: StorageKey): string | null => {
+//   const storedValue = storage.getString(key);
+//   if (!storedValue) return null;
 
-  try {
-    const parsedData = JSON.parse(storedValue);
-    console.error(' parsedData parsing MMKV data:', parsedData);
-    // Extract the token dynamically (handling varying key names)
-    const sessionKey = Object.keys(parsedData)[0]; // Get first key
-    return parsedData[sessionKey]; // Return the actual token value
-  } catch (error) {
-    console.error('❌ Error parsing MMKV data:', error);
-    return null;
-  }
-};
+//   try {
+//     const parsedData = JSON.parse(storedValue);
+//     console.error(' parsedData parsing MMKV data:', parsedData);
+//     // Extract the token dynamically (handling varying key names)
+//     const sessionKey = Object.keys(parsedData)[0]; // Get first key
+//     return parsedData[sessionKey]; // Return the actual token value
+//   } catch (error) {
+//     console.error('❌ Error parsing MMKV data:', error);
+//     return null;
+//   }
+// };
 
-export const removeItem = (key: StorageKey): void => storage.delete(key);
+// export const removeItem = (key: StorageKey): void => storage.delete(key);
